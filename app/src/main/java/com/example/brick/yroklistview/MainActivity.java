c11a4afc56e1487341a9995574644fb845cf1e8c
brick8 <brick84@yandex.ru>
2017-11-05 14:18:46 +0300

    4eto sdelal

--- app/src/main/java/com/example/brick/yroklistview/MainActivity.java
+++ app/src/main/java/com/example/brick/yroklistview/MainActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
-
+    private TransactionAdapter transactionAdapter;
+    List<Transactions> data = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView)findViewById(R.id.listview);
+        List<Transactions> adapterData = getDataList();
+        transactionAdapter = new TransactionAdapter(MainActivity.this, adapterData);
+        listView = ( ListView )findViewById(R.id.listview);
+        listView.setAdapter(transactionAdapter);
+
+    }
+    private List<Transactions> getDataList(){
+        data.add(new Transactions("Thelephone", "2000"));
+        data.add(new Transactions("T-Shirts", "3000"));
+        data.add(new Transactions("Jeans", "1000"));
+
+        return data;
     }
 }
--- app/src/main/java/com/example/brick/yroklistview/TransactionAdapter.java
+++ app/src/main/java/com/example/brick/yroklistview/TransactionAdapter.java
@@ -2,7 +2,12 @@ package com.example.brick.yroklistview;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -12,7 +17,22 @@ import java.util.List;
 
 public class TransactionAdapter extends ArrayAdapter<Transactions>{
 
-    public TransactionAdapter(Context context, List<Transactions> transactionses) {
-        super(context,0, transactionses);
+
+    public TransactionAdapter(@NonNull Context context, @NonNull List<Transactions> transactionses) {
+        super(context,0,  transactionses);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Transactions transactions = getItem(position);
+        if(convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, null);
+        }
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        TextView sum = (TextView) convertView.findViewById(R.id.sum);
+        name.setText(transactions.name);
+        sum.setText(transactions.sum);
+        return convertView;
     }
 }
